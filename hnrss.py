08493f6118eab2e6468f6aa676ba208b4e624594
Eric Davis <eric@davising.com>
2014-06-08 12:28:51 -0700

    Migrate "simple" feeds to class-based system

--- hnrss.py
+++ hnrss.py
@@ -14,11 +14,17 @@ class API(object):
         self.endpoint = endpoint
         self.params = {}
         if points or comments:
-            numeric_filters = filter(bool, [
-                'points>%s' % points if points else None,
-                'num_comments>%s' % comments if comments else None,
-            ])
+            numeric_filters = []
+            if points: numeric_filters.append('points>%s' % points)
+            if comments: numeric_filters.append('num_comments>%s' % comments)
             self.params['numericFilters'] = ','.join(numeric_filters)
+
+    @classmethod
+    def using_request(cls, request):
+        return cls(
+            points = request.args.get('points'),
+            comments = request.args.get('comments'),
+        )
 
     def _request(self, tags, query=None):
         params = self.params.copy()
@@ -45,157 +51,91 @@ class API(object):
     def polls(self):
         return self._request('poll')
 
-def do_search(request, endpoint, tags):
-    params = {}
-    filters = []
+class RSS(object):
+    def __init__(self, api_response, title, link='https://news.ycombinator.com/'):
+        self.api_response = api_response
 
-    if tags:
-        params['tags'] = tags
+        self.rss_root = etree.Element('rss', version='2.0')
+        self.rss_channel = etree.SubElement(self.rss_root, 'channel')
 
-    if request.args.get('query'):
-        params['query'] = '"%s"' % request.args.get('query')
+        self.add_element(self.rss_channel, 'title', title)
+        self.add_element(self.rss_channel, 'link', link)
+        self.add_element(self.rss_channel, 'description', 'Hacker News RSS')
+        self.add_element(self.rss_channel, 'docs', 'http://cyber.law.harvard.edu/rss/rss.html')
+        self.add_element(self.rss_channel, 'generator', 'https://github.com/edavis/hnrss')
+        self.add_element(self.rss_channel, 'lastBuildDate', self.generate_rfc2822())
 
-    if request.args.get('points'):
-        filters.append('points>%s' % request.args.get('points'))
-    if request.args.get('comments'):
-        filters.append('num_comments>%s' % request.args.get('comments'))
-    if filters:
-        params['numericFilters'] = ','.join(filters)
+        self.generate_body()
 
-    return requests.get('%s/%s' % (API_BASE_URL, endpoint),
-                        params=params, verify=False)
+    def generate_body(self):
+        for hit in self.api_response['hits']:
+            rss_item = etree.SubElement(self.rss_channel, 'item')
+            hn_url = 'https://news.ycombinator.com/item?id=%s' % hit['objectID']
+            tags = hit.get('_tags', [])
 
-def add_element(parent, tag, value, **kwargs):
-    el = etree.Element(tag, kwargs)
-    el.text = value
-    parent.append(el)
-    return el
-
-def generate_rfc2822(secs=None):
-    t = time.gmtime(secs)
-    return time.strftime('%a, %d %b %Y %H:%M:%S GMT', t)
-    
-def generate_rss(request, response, title):
-    """
-    Generate a RSS document from a search API response.
-    """
-    rss_root = etree.Element('rss', version='2.0')
-    rss_channel = etree.SubElement(rss_root, 'channel')
-
-    add_element(rss_channel, 'title', title)
-    add_element(rss_channel, 'link', 'https://news.ycombinator.com/')
-    add_element(rss_channel, 'description', 'Hacker News RSS')
-    add_element(rss_channel, 'docs', 'http://cyber.law.harvard.edu/rss/rss.html')
-    add_element(rss_channel, 'generator', 'https://github.com/edavis/hnrss')
-    add_element(rss_channel, 'lastBuildDate', generate_rfc2822())
-
-    for hit in response.json()['hits']:
-        rss_item = etree.SubElement(rss_channel, 'item')
-        hn_url = 'https://news.ycombinator.com/item?id=%s' % hit['objectID']
-        tags = hit.get('_tags', [])
-
-        if 'comment' in tags:
-            add_element(rss_item, 'title', 'New comment by %s in "%s"' % (
-                hit.get('author'), hit.get('story_title')))
-            add_element(rss_item, 'description', hit.get('comment_text'))
-        else:
-            if hit.get('title'):
-                add_element(rss_item, 'title', hit.get('title'))
-            if hit.get('story_text'):
-                add_element(rss_item, 'description', hit.get('story_text'))
-
-        add_element(rss_item, 'pubDate', generate_rfc2822(hit.get('created_at_i')))
-
-        if ('ask_hn' in tags or 'poll' in tags or 'comment' in tags):
-            add_element(rss_item, 'link', hn_url)
-        elif 'story' in tags:
-            if request.args.get('link') == 'comments':
-                add_element(rss_item, 'link', hn_url)
-            elif hit.get('url'):
-                add_element(rss_item, 'link', hit.get('url'))
+            if 'comment' in tags:
+                self.add_element(rss_item, 'title', 'New comment by %s in "%s"' % (
+                    hit.get('author'), hit.get('story_title')))
+                self.add_element(rss_item, 'description', hit.get('comment_text'))
             else:
-                add_element(rss_item, 'link', hn_url)
-        
-        add_element(rss_item, 'author', hit.get('author'))
+                if hit.get('title'):
+                    self.add_element(rss_item, 'title', hit.get('title'))
+                if hit.get('story_text'):
+                    self.add_element(rss_item, 'description', hit.get('story_text'))
 
-        if ('story' in tags or 'poll' in tags):
-            add_element(rss_item, 'comments', hn_url)
+            self.add_element(rss_item, 'pubDate', self.generate_rfc2822(hit.get('created_at_i')))
 
-        add_element(rss_item, 'guid', hn_url)
+            if ('ask_hn' in tags or 'poll' in tags or 'comment' in tags):
+                self.add_element(rss_item, 'link', hn_url)
+            elif 'story' in tags:
+                self.add_element(rss_item, 'link', hit.get('url') or hn_url)
 
-    return rss_root
+            self.add_element(rss_item, 'author', hit.get('author'))
 
-def make_rss_response(rss_doc):
-    rss_xml = etree.tostring(rss_doc, pretty_print=True, encoding='UTF-8', xml_declaration=True)
-    return (rss_xml, 200, {'Content-Type': 'text/xml'})
+            if ('story' in tags or 'poll' in tags):
+                self.add_element(rss_item, 'comments', hn_url)
+
+            self.add_element(rss_item, 'guid', hn_url)
+
+    def add_element(self, parent, tag, text):
+        el = etree.Element(tag)
+        el.text = text
+        parent.append(el)
+        return el
+
+    def generate_rfc2822(self, secs=None):
+        t = time.gmtime(secs)
+        return time.strftime('%a, %d %b %Y %H:%M:%S GMT', t)
+
+    def response(self):
+        rss_xml = etree.tostring(
+            self.rss_root, pretty_print=True, encoding='UTF-8', xml_declaration=True,
+        )
+        return (rss_xml, 200, {'Content-Type': 'text/xml'})
 
 @app.route('/feeds/firehose.xml')
-def stories():
-    response = do_search(request, 'search_by_date', '(story,poll)')
-    rss = generate_rss(request, response, 'Hacker News: Firehose')
-    return make_rss_response(rss)
+def firehose():
+    api = API.using_request(request)
+    rss = RSS(api.firehose(), 'Hacker News: Firehose', 'https://news.ycombinator.com/newest')
+    return rss.response()
 
 @app.route('/feeds/askhn.xml')
 def askhn():
-    response = do_search(request, 'search_by_date', 'ask_hn')
-    rss = generate_rss(request, response, 'Hacker News: Ask HN')
-    return make_rss_response(rss)
+    api = API.using_request(request)
+    rss = RSS(api.ask_hn(), 'Hacker News: Ask HN', 'https://news.ycombinator.com/ask')
+    return rss.response()
 
 @app.route('/feeds/showhn.xml')
 def showhn():
-    response = do_search(request, 'search_by_date', 'show_hn')
-    rss = generate_rss(request, response, 'Hacker News: Show HN')
-    return make_rss_response(rss)
+    api = API.using_request(request)
+    rss = RSS(api.show_hn(), 'Hacker News: Show HN')
+    return rss.response()
 
 @app.route('/feeds/polls.xml')
 def polls():
-    response = do_search(request, 'search_by_date', 'poll')
-    rss = generate_rss(request, response, 'Hacker News: Polls')
-    return make_rss_response(rss)
-
-@app.route('/feeds/comments.xml')
-def comments():
-    tags = ['comment']
-    if request.args.get('id'):
-        tags.append('story_%s' % request.args.get('id'))
-        response = do_search(request, 'search_by_date', ','.join(tags))
-        if response.json()['hits']:
-            rss_title = '"%s" comments' % response.json()['hits'][0]['story_title']
-        else:
-            rss_title = 'New Comments'
-    else:
-        response = do_search(request, 'search_by_date', ','.join(tags))
-        rss_title = 'New Comments'
-    rss = generate_rss(request, response, 'Hacker News: %s' % rss_title)
-    return make_rss_response(rss)
-
-@app.route('/feeds/author/<username>.xml')
-def author(username):
-    tags = ['author_%s' % username]
-    if request.args.get('only'):
-        only = request.args.get('only')
-        if only == 'stories':
-            tags.append('story')
-        elif only == 'comments':
-            tags.append('comment')
-    response = do_search(request, 'search_by_date', ','.join(tags))
-    rss = generate_rss(request, response, 'Hacker News: %s RSS feed' % username)
-    return make_rss_response(rss)
-
-@app.route('/feeds/search.xml')
-def search():
-    query = request.args.get('query')
-    tags = 'story'
-    if request.args.get('all'):
-        tags = '(story,comment)'
-    response = do_search(request, 'search_by_date', tags)
-    rss = generate_rss(request, response, 'Hacker News: Search for "%s"' % query)
-    return make_rss_response(rss)
-
-@app.route('/')
-@app.route('/feeds/')
-def index():
-    return redirect('https://github.com/edavis/hnrss')
+    api = API.using_request(request)
+    rss = RSS(api.polls(), 'Hacker News: Polls')
+    return rss.response()
 
 if __name__ == '__main__':
     app.run(debug=True)

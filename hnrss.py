79da377abb8079cb6189f07bc428d8f844c884eb
Eric Davis <edavis@eresources.com>
2017-02-15 16:39:37 -0800

    Add gzip support

--- hnrss.py
+++ hnrss.py
@@ -2,10 +2,12 @@
 
 from api import API
 from flask import Flask, request, redirect
+from flask_compress import Compress
 from rss import RSS
 from urllib import urlencode
 
 app = Flask(__name__)
+Compress(app)
 
 @app.route('/newest')
 def newest():

66030263821935537c45e627b96f2b42f26d7181
Eric Davis <edavis@eresources.com>
2020-11-15 14:48:55 -0800

    Add endpoints for Best, Invited, Active, and Launches

---
+++ special.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"astuart.co/goq"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func Special(url, title string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var sp SearchParams
+		var op OutputParams
+		ParseRequest(c, &sp, &op)
+
+		resp, err := algoliaClient.Get(url)
+		if err != nil {
+			c.Error(err)
+			c.String(http.StatusBadGateway, err.Error())
+			return
+		}
+		defer resp.Body.Close()
+
+		var parsed ItemList
+		err = goq.NewDecoder(resp.Body).Decode(&parsed)
+		if err != nil {
+			c.Error(err)
+			c.String(http.StatusBadGateway, err.Error())
+			return
+		}
+
+		var sids []string
+		for _, id := range parsed.Thing {
+			sids = append(sids, "story_"+id)
+		}
+		sp.Tags = fmt.Sprintf("story,(%s)", strings.Join(sids, ","))
+		sp.Count = strconv.Itoa(len(sids))
+
+		op.Title = title
+		op.Link = url
+
+		Generate(c, &sp, &op)
+	}
+}
